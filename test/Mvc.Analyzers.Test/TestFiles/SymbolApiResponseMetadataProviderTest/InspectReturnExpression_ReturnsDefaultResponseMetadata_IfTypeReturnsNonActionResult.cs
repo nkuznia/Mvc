a19da926041701ed8ae78db38280b405fc430488
Pranav K <prkrishn@hotmail.com>
2018-07-12 09:24:03 -0700

    Refactorings for codefix

---
+++ test/Mvc.Analyzers.Test/TestFiles/SymbolApiResponseMetadataProviderTest/InspectReturnExpression_ReturnsDefaultResponseMetadata_IfTypeReturnsNonActionResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.AspNetCore.Mvc.Analyzers
+{
+    public class InspectReturnExpression_ReturnsDefaultResponseMetadata_IfTypeReturnsNonActionResult : ControllerBase
+    {
+        public InspectReturnExpression_ReturnsDefaultResponseMetadata_IfTypeReturnsNonActionResultModel Get()
+        {
+            return new InspectReturnExpression_ReturnsDefaultResponseMetadata_IfTypeReturnsNonActionResultModel();
+        }
+    }
+
+    public class InspectReturnExpression_ReturnsDefaultResponseMetadata_IfTypeReturnsNonActionResultModel { }
+}
